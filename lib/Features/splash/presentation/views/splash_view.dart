b688c2ffccfb333663d0e3949c8ad78ef2f20962
gelkomyy <string.cf@gmail.com>
2024-02-07 03:43:26 +0200

    add splash screen

---
+++ lib/Features/splash/presentation/views/splash_view.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:snap_text/Features/home/presentation/views/home_view.dart';
+import 'package:snap_text/constans.dart';
+import 'package:snap_text/core/models/image_model.dart';
+import 'package:snap_text/core/utils/languages_enum.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    _animation =
+        CurvedAnimation(parent: _animationController, curve: Curves.easeInOut)
+          ..addListener(() {
+            setState(() {});
+          });
+    _animationController.forward();
+
+    cInitHive().then((value) {
+      setState(() {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => const HomeView()),
+        );
+      });
+    });
+  }
+
+  Future<void> cInitHive() async {
+    Hive.registerAdapter(LanguagesEnumAdapter());
+    Hive.registerAdapter(ImageModelAdapter());
+    await Hive.initFlutter();
+    await Hive.openBox<ImageModel>(historyBox);
+
+    await Hive.openBox<ImageModel>(bookmarkBox);
+    await Future.delayed(const Duration(seconds: 4));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kPrimaryColor,
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.lightBlueAccent,
+              kPrimaryColor
+            ], // Set your gradient colors
+            begin: Alignment.topLeft, // Optional, defaults to top center
+            end: Alignment.bottomRight, // Optional, defaults to bottom center
+            //   stops: [ 0.4, 0.3], // Optional, specify the stop points
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              // Replace 'assets/logo.png' with the path to your PNG logo
+              Image.asset(
+                'assets/logo.png',
+                width: 250 * _animation.value,
+                height: 250 * _animation.value,
+              ),
+
+              const SizedBox(height: 20),
+              const CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.white70),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 import 'package:snap_text/Features/home/presentation/manager/database_manager_cubit/database_manager_cubit.dart';
 import 'package:snap_text/Features/home/presentation/manager/get_bookmark_items_cubit/get_bookmark_items_cubit.dart';
 import 'package:snap_text/Features/home/presentation/manager/get_history_items_cubit/get_history_items_cubit.dart';
-import 'package:snap_text/Features/home/presentation/views/home_view.dart';
-import 'package:snap_text/constans.dart';
-import 'package:snap_text/core/models/image_model.dart';
+import 'package:snap_text/Features/splash/presentation/views/splash_view.dart';
 import 'package:snap_text/core/utils/custom_bloc_observer.dart';
-import 'package:snap_text/core/utils/languages_enum.dart';
 
 void main() async {
   Bloc.observer = CustomBlocObserver();
-  Hive.registerAdapter(LanguagesEnumAdapter());
-  Hive.registerAdapter(ImageModelAdapter());
-  await Hive.initFlutter();
-  await Hive.openBox<ImageModel>(historyBox);
-
-  await Hive.openBox<ImageModel>(bookmarkBox);
 
   runApp(const SnapText());
 }
@@ -41,7 +31,7 @@ class SnapText extends StatelessWidget {
       ],
       child: const MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: HomeView(),
+        home: SplashScreen(),
       ),
     );
   }
