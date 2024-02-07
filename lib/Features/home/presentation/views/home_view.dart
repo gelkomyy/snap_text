944c172a2fcd390f8a7503f46473ccf3b4fef854
gelkomyy <string.cf@gmail.com>
2024-02-07 05:17:19 +0200

    add CustomBottomNavBar

--- lib/Features/home/presentation/views/home_view.dart
+++ lib/Features/home/presentation/views/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:snap_text/Features/home/presentation/manager/get_history_items_cubit/get_history_items_cubit.dart';
 import 'package:snap_text/Features/home/presentation/views/widgets/home_view_body.dart';
 import 'package:snap_text/constans.dart';
@@ -21,8 +22,65 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      bottomNavigationBar: CustomBottomNavBar(),
       backgroundColor: kBackgroundColor,
       body: SafeArea(child: HomeViewBody()),
+    );
+  }
+}
+
+class CustomBottomNavBar extends StatelessWidget {
+  const CustomBottomNavBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.black,
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 20,
+            color: Colors.white.withOpacity(.1),
+          )
+        ],
+      ),
+      child: const SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+          child: GNav(
+            rippleColor: Color(0xff232023),
+            hoverColor: kPrimaryColor,
+            gap: 8,
+            activeColor: Colors.white,
+            iconSize: 24,
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+            duration: Duration(milliseconds: 400),
+            tabBackgroundColor: Color(0xff2C2E2F),
+            color: Colors.white,
+            tabs: [
+              GButton(
+                icon: Icons.home,
+                text: 'Home',
+              ),
+              GButton(
+                icon: Icons.favorite,
+                text: 'Likes',
+              ),
+              GButton(
+                icon: Icons.search,
+                text: 'Search',
+              ),
+              GButton(
+                icon: Icons.person,
+                text: 'Profile',
+              ),
+            ],
+            // selectedIndex: _selectedIndex,
+          ),
+        ),
+      ),
     );
   }
 }
